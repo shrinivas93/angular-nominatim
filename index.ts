3c578d6a4cf304bb17de189150f8ecc3f5959055
Shrinivas Shukla <ssshukla1993@gmail.com>
2017-11-14 21:01:21 +0530

    * Implemented service

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+
 export { NominatimService } from "./src/nominatim.service";
 export { GeocodeConfig } from "./src/geocode.config";
 export { ReverseGeocodeConfig } from "./src/reversegeocode.config";
 export { GeocodeResponse } from "./src/geocode.response";
 export { ReverseGeocodeResponse } from "./src/reversegeocode.response";
+
+@NgModule({
+    imports: [HttpClientModule]
+})
+export class NominatimModule { }
--- src/nominatim.service.ts
+++ src/nominatim.service.ts
@@ -1,19 +1,58 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 import { NominatimConstant } from "./nominatim.constant";
 import { GeocodeConfig } from "./geocode.config";
 import { ReverseGeocodeConfig } from "./reversegeocode.config";
 import { GeocodeResponse } from "./geocode.response";
 import { ReverseGeocodeResponse } from "./reversegeocode.response";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class NominatimService {
-  constructor(private http: HttpClient) {}
-  getLocation(config: GeocodeConfig): GeocodeResponse {
-    return null;
+
+  constructor(private http: HttpClient) {
+
   }
-  getAddress(config: ReverseGeocodeConfig): ReverseGeocodeResponse {
-    return null;
+
+  getLocation(config: GeocodeConfig): Observable<GeocodeResponse[]> {
+    let q = config.q || "";
+    let addressdetails = config.addressdetails;
+    if (addressdetails != 0 && addressdetails != 1) {
+      addressdetails = 1;
+    }
+    let polygon_geojson = config.polygon_geojson;
+    if (polygon_geojson != 0 && polygon_geojson != 1) {
+      polygon_geojson = 1;
+    }
+    let params = new HttpParams();
+    params.set("q", q);
+    params.set("addressdetails", addressdetails.toString());
+    params.set("polygon_geojson", polygon_geojson.toString());
+    return this.http.get<GeocodeResponse[]>(NominatimConstant.GEOCODING_ENDPOINT, {
+      params: params
+    });
+  }
+
+  getAddress(config: ReverseGeocodeConfig): Observable<ReverseGeocodeResponse> {
+    let lat = config.lat || "";
+    let lon = config.lon || "";
+    let addressdetails = config.addressdetails;
+    if (addressdetails != 0 && addressdetails != 1) {
+      addressdetails = 1;
+    }
+    let polygon_geojson = config.polygon_geojson;
+    if (polygon_geojson != 0 && polygon_geojson != 1) {
+      polygon_geojson = 1;
+    }
+    let params = new HttpParams();
+    params.set("lat", lat.toString());
+    params.set("lon", lon.toString());
+    params.set("addressdetails", addressdetails.toString());
+    params.set("polygon_geojson", polygon_geojson.toString());
+    return this.http.get<ReverseGeocodeResponse>(NominatimConstant.REVERSE_GEOCODING_ENDPOINT, {
+      params: params
+    });
   }
 }
