edd92e4b643a444fd82ea1606a9585c02eeb5aa6
Jan-Henning Wegener <jhwegener@users.noreply.github.com>
2018-04-11 15:46:44 +0200

    add fixes in ts files

---
+++ nominatim.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpParams } from "@angular/common/http";
+
+import { NominatimConstant } from "./nominatim.constant";
+import { GeocodeConfig } from "./geocode.config";
+import { ReverseGeocodeConfig } from "./reversegeocode.config";
+import { GeocodeResponse } from "./geocode.response";
+import { ReverseGeocodeResponse } from "./reversegeocode.response";
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class NominatimService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getLocation(config: GeocodeConfig): Observable<GeocodeResponse[]> {
+    let q = config.q || "";
+    let addressdetails = config.addressdetails;
+    if (addressdetails != 0 && addressdetails != 1) {
+      addressdetails = 1;
+    }
+    let polygon_geojson = config.polygon_geojson;
+    if (polygon_geojson != 0 && polygon_geojson != 1) {
+      polygon_geojson = 1;
+    }
+    let params = new HttpParams()
+        .set("format", "json")
+        .set("q", q)
+        .set("addressdetails", addressdetails.toString())
+        .set("polygon_geojson", polygon_geojson.toString());
+    return this.http.get<GeocodeResponse[]>(NominatimConstant.GEOCODING_ENDPOINT, {
+      params: params
+    });
+  }
+
+  getAddress(config: ReverseGeocodeConfig): Observable<ReverseGeocodeResponse> {
+    let lat = config.lat || "";
+    let lon = config.lon || "";
+    let addressdetails = config.addressdetails;
+    if (addressdetails != 0 && addressdetails != 1) {
+      addressdetails = 1;
+    }
+    let polygon_geojson = config.polygon_geojson;
+    if (polygon_geojson != 0 && polygon_geojson != 1) {
+      polygon_geojson = 1;
+    }
+    let params = new HttpParams()
+        .set("format", "json")
+        .set("lat", lat.toString())
+        .set("lon", lon.toString())
+        .set("addressdetails", addressdetails.toString())
+        .set("polygon_geojson", polygon_geojson.toString());
+    return this.http.get<ReverseGeocodeResponse>(NominatimConstant.REVERSE_GEOCODING_ENDPOINT, {
+      params: params
+    });
+  }
+}
